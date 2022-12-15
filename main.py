7dd6c4d7feb394b31b1db210d9dc277b189af5e3
ntorba <32570754+ntorba@users.noreply.github.com>
2022-12-15 15:29:07 -0500

    first commit

---
+++ main.py
@@ -0,0 +1,57 @@
+import os
+import requests
+import pandas as pd
+from twarc import Twarc2
+import streamlit as st
+import plotly.express as px
+from dotenv import load_dotenv
+
+load_dotenv()
+
+twarc = Twarc2(bearer_token=os.environ["TWITTER_TOKEN"])
+
+st.title("See the Borg Community Landscape of who you follow on twitter")
+
+# streamlit dropdown of usernames for any users that have two csv's in the data/ directory
+usernames = list(set([file.split("_")[0] for file in os.listdir("data/") if file.endswith(".csv")]))
+username = st.selectbox("Select a username", usernames)
+
+# load both csv's into pandas dataframes
+following_df = pd.read_csv(f"data/{username}_following.csv")
+borg_community_df = pd.read_csv(f"data/{username}_borg_community_info.csv")
+
+# streamlit table that displays the dataframe
+st.write(f'{username} follows {len(following_df)} users')
+
+# streamlit button that allows me to download the csv
+st.download_button(label=f"Download CSV of who {username} follows", data=following_df.to_csv(index=False), file_name="data/{username}_following.csv", mime="text/csv")
+st.download_button(label=f"Download CSV community aggregation for {username}", borg_community_df=following_df.to_csv(index=False), file_name="data/{username}_following.csv", mime="text/csv")
+
+st.write(f'{borg_community_df[borg_community_df["clusters.name"].isna()].shape[0]} users you follow are not included in any clusters.')
+
+st.write(f'Users you follow are included in {borg_community_df["clusters.name"].nunique()} unique communities.')
+
+
+# streamlit select range of top communities to display
+
+
+min_num_communities, max_num_communities = st.slider("Select number of top communities to display", min_value=0, max_value=borg_community_df["clusters.name"].nunique(), value=(0,20), step=5)
+community_grouping = borg_community_df.groupby('clusters.name').agg({'username': 'count'}, dropna=False).reset_index().sort_values('username', ascending=False)
+
+
+fig = px.bar(
+    community_grouping.iloc[min_num_communities:max_num_communities], 
+    x="clusters.name", 
+    y="username", 
+    color="clusters.name", 
+    title=f"Num users in hive community"
+)
+st.plotly_chart(fig)
+
+
+st.subheader("Show users by community")
+community = st.selectbox("Select a community", community_grouping['clusters.name'].unique())
+specific_community = borg_community_df[borg_community_df['clusters.name'] == community][["username", "latest_scores.rank"]].sort_values("latest_scores.rank", ascending=True)
+specific_community["latest_scores.rank"] = specific_community["latest_scores.rank"].astype(int)
+st.write(f"You follow {specific_community.shape[0]} users in the {community} community, with an average rank of {round(specific_community['latest_scores.rank'].mean())}")
+st.table(specific_community)
---
+++ save_following.py
@@ -0,0 +1,92 @@
+
+
+import os
+import concurrent.futures
+import requests
+import pandas as pd
+from twarc import Twarc2
+from twarc_csv import DataFrameConverter
+import streamlit as st
+import click
+from dotenv import load_dotenv
+
+load_dotenv()
+
+twarc = Twarc2(bearer_token=os.environ["TWITTER_TOKEN"])
+converter = DataFrameConverter(input_data_type="users", allow_duplicates=False)   
+
+BORG_API_ENDPOINT = "https://api.borg.id/influence/influencers/twitter:{}/"
+
+# create click group
+@click.group()
+def cli():
+    pass
+
+# use twarc to get the users a user follows
+def get_following(user_id):
+    following = []
+    for page in twarc.following(user_id):
+        page_df = converter.process([page])
+        following.append(page_df)
+    return pd.concat(following)
+
+# save the users that nicktorba follows to a csv
+def save_following_to_csv(user_id, username):
+    following_df = get_following(user_id)
+    following_df.to_csv(f"data/{username}_following.csv", index=False)
+    return following_df
+
+# function that makes a request to the borg api, with the user id inserted, and returns the response
+def get_borg_influence(user):
+    # make borg request with BORG_API_KEY env var included in request headers 
+    response = requests.get(BORG_API_ENDPOINT.format(user["id"]), headers={"Authorization": f'Token {os.environ["BORG_API_KEY"]}'})
+    return user, response.json()
+
+def get_cluster_info(df):
+    df_rows = []
+    # use conncurrent.futures to make requests to the borg api in parallel
+    with concurrent.futures.ThreadPoolExecutor() as executor:
+        futures = [executor.submit(get_borg_influence, user) for user in df.to_dict(orient="records")]
+        for future in concurrent.futures.as_completed(futures):
+            user, borg_influence = future.result()
+            if 'error' in borg_influence:
+                print(f"user {user['username']} is not indexed by borg")
+                df_rows.append(user)
+                continue
+            clusters = borg_influence.get("clusters", [])
+            if len(clusters) < 0:
+                # get the row from df where id == user_id as a dict
+                df_rows.append(user)
+                continue
+
+            clusters_by_id = {cluster["id"]: cluster for cluster in clusters}
+
+            latest_scores = borg_influence["latest_scores"]
+
+            for score_dict in latest_scores:
+                cluster_id = score_dict["cluster_id"]
+                score_dict = {f'latest_scores.{key}': value for key, value in score_dict.items()}
+                cluster = clusters_by_id[cluster_id]
+                cluster = {f'clusters.{key}': value for key, value in cluster.items()}
+                row = {**score_dict, **cluster, **user}
+                df_rows.append(row)
+    return pd.DataFrame(df_rows)
+
+
+
+# a click function to save the users that a user follows to a csv
+@cli.command()
+@click.option("--username", default="nicktorba", help="The name of the user whose following you want to save")
+def save_following(username):
+    # use twarc to get the user_id from username
+    gen = twarc.user_lookup(users=[username], usernames=True) 
+    data = [i for i in gen]
+    user_id = data[0]["data"][0]["id"]
+    df = save_following_to_csv(user_id, username)
+    com_df = get_cluster_info(df)
+    # use twarc to get the user_id from username
+    com_df.to_csv(f"data/{username}_borg_community_info.csv", index=False)
+
+    
+if __name__ == "__main__":
+    cli()
