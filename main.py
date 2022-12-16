d9899c1d6ac741d6a00042c722fd0fe739b0daa5
ntorba <32570754+ntorba@users.noreply.github.com>
2022-12-15 22:41:11 -0500

    Update main.py

--- main.py
+++ main.py
@@ -18,10 +18,24 @@ st.title("See the Borg Community Landscape of who you follow on twitter")
 usernames = list(set([file.split("_")[0] for file in os.listdir("data/") if file.endswith(".csv")]))
 # username = st.selectbox("Select a username", usernames)
 
+username_select_box = st.selectbox("See distribution for user who has already been loaded", [None] + usernames)
+
 # get streamlit text input
-username = st.text_input("Enter a username", value="nicktorba")
+username_text_input = st.text_input("Enter a twitter username for a user not availble in the dropdown", value=username_select_box)
+
+username = None
+if username_select_box is not None:
+    username = username_select_box
+elif username_text_input is not None:
+    username = username_text_input
+
+if username == "None": # TODO: wtf is happening here..?
+    st.write("Please select a username from the dropdown or enter a username in the text box")
+    st.stop()
+
 gen = twarc.user_lookup(users=[username], usernames=True) 
 data = [i for i in gen][0]
+
 if "errors" in data:
     st.write(f"User {username} not found, please try again.")
 else: 
@@ -34,7 +48,7 @@ else:
         # create a streamlit progress bar
         with st.spinner("Loading follows from twitter"):
             following_df = save_following_to_csv(user_id, username)
-        with st.spinner("Loading community info from borg"):
+        with st.spinner("Loading community info from borg (this can take some time)"):
             borg_community_df = get_cluster_info(following_df)
             borg_community_df.to_csv(f"data/{username}_borg_community_info.csv", index=False)
 
@@ -95,7 +109,7 @@ else:
     st.dataframe(min_group)
 
     # select username from dropdown 
-    username = st.selectbox("Select a username to see which communities they belong to", min_group["username"].unique())
+    investigate_username = st.selectbox("Select a username to see which communities they belong to", min_group["username"].unique())
     # get the communities that user belongs in
-    user_communities = borg_community_df[borg_community_df["username"] == username]["clusters.name"].unique()
+    user_communities = borg_community_df[borg_community_df["username"] == investigate_username]["clusters.name"].unique()
     st.write(user_communities)
